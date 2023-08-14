aafd9f24e0060a64cbf9bee09f78bf0186fd3115
NgVBinh <nguyenvanbinh06102002@gmail.com>
2023-08-14 20:50:13 +0700

    update lesson4

---
+++ Learn_Unity/Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+   // [SerializeField] protected GameObject target;
+
+    public Transform targetObject;
+    public float smooth = 2f;
+    public Vector3 cameraOffset; // Khoảng cách theo trục y và z
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        Vector3 newCameraPosition = targetObject.position + targetObject.forward * cameraOffset.z + targetObject.up * cameraOffset.y;
+        Vector3 cameraPosition = Vector3.Lerp(transform.position, newCameraPosition, smooth*Time.deltaTime);
+        transform.position = cameraPosition;
+
+        transform.LookAt(targetObject);
+    }
+}
---
+++ Learn_Unity/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+enum targetEnum
+{
+    topLeft,
+    topRight,
+    bottomLeft,
+    bottomRight
+}
+
+enum DriveMode
+{
+    Automatic,
+    Manual,
+}
+public class PlayerController : MonoBehaviour
+{
+    public Transform topLeftTarget;
+    public Transform topRightTarget;
+    public Transform bottomLeftTarget;
+    public Transform bottomRightTarget;
+
+    public int Speed = 10;
+    public float speedRotate = 100f;
+    public bool check;
+
+    private Transform currentTarget;
+    private targetEnum nextTarget= targetEnum.topLeft;
+
+    private DriveMode mode= DriveMode.Manual;
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTarget = topLeftTarget;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (mode == DriveMode.Manual)
+        {
+            Manual();
+        }
+        else
+        {
+            AutoDrive();
+        }
+        SetMode();
+    }
+
+    private void SetMode()
+    {
+        
+        if (check)
+        {
+            mode = DriveMode.Automatic;
+        }
+        else
+        {
+            mode = DriveMode.Manual;
+        }
+
+        check = Input.GetKey(KeyCode.A);
+    }
+    private void AutoDrive()
+    {
+        Vector3 targetPosition = currentTarget.position;
+        Vector3 moveDirection = targetPosition - transform.position;
+        float distance = (moveDirection).magnitude;
+
+        if (distance > 0.1f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, currentTarget.position, Speed * Time.deltaTime);
+        }
+        else
+        {
+            setNextTarget(nextTarget);
+        }
+
+        Vector3 direction = currentTarget.position - transform.position;
+        Quaternion targetRotation = Quaternion.LookRotation(direction, Vector3.up);
+        transform.rotation = targetRotation;
+    }
+    private void setNextTarget(targetEnum target)
+    {
+        switch (target)
+        {
+            case targetEnum.topLeft:
+                currentTarget = topLeftTarget;
+                nextTarget = targetEnum.topRight;
+                break;
+            case targetEnum.topRight:
+                currentTarget = topRightTarget;
+                nextTarget = targetEnum.bottomRight;
+                break;
+            case targetEnum.bottomRight:
+                currentTarget = bottomRightTarget;
+                nextTarget = targetEnum.bottomLeft;
+                break;
+            case targetEnum.bottomLeft:
+                currentTarget = bottomLeftTarget;
+                nextTarget = targetEnum.topLeft;
+                break;
+        }
+    }
+
+    private void Manual()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(0f,0f, vertical)*Speed*Time.deltaTime; 
+        transform.Translate(movement);
+
+        float rotationAmount = horizontal * speedRotate * Time.deltaTime;
+        // Tạo một vector quay quanh trục Y
+        Vector3 rotationVector = new Vector3(0f, rotationAmount, 0f);
+
+        // Áp dụng quay vào transform của đối tượng
+        transform.Rotate(rotationVector);
+    }
+}
